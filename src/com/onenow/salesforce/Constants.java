7af99eca58e90c28188c5b3c04601e5f4c3c1f1a
Pablo Rodriguez Bertorello <pablo.bertorello@gmail.com>
2014-12-22 17:51:51 -0800

    Salesforce changes

---
+++ src/com/onenow/salesforce/Constants.java
@@ -0,0 +1,10 @@
+package com.onenow.salesforce;
+
+public class Constants {
+
+	final static String USERNAME = "pablo.personal@icloud.com";
+	final static String PASSWORD = "p*blo433"; 
+	final static String TOKEN = "oZsiDH5iH8HKyyHUqrHAfCZOy";
+	final static String URL = "https://login.salesforce.com/services/Soap/c/32.0";
+
+}
