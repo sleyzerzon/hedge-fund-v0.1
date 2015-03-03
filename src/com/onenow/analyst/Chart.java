a46a27a3ebc1b4b9776cfad3aa2b9d8dec6854b8
Pablo Rodriguez Bertorello <pablo.bertorello@gmail.com>
2015-03-03 11:26:21 -0300

    chart analysis set and breakdown of get

--- src/com/onenow/analyst/Chart.java
+++ src/com/onenow/analyst/Chart.java
@@ -21,133 +21,44 @@ public class Chart {
 //	Stats VWAPSpreadStats;
 //	Stats VWAPSpreadToSizeRatioStats;
 	
-	
+
+	// TODO take only complete candles; ignore opening / close high volume?
 	public Chart() {
 	}
 	
 	// PUBLIC	
-	public String getAnalysis(int which) {
-		String s = "";
-		
+	public void setAnalysis() {
 		setPriceOnlyAnalysis(new AnalysisPriceOnly(prices));
 		setVolumePriceAnalysis(new AnalysisVolumePrice(prices, sizes, priceOnlyAnalysis));
-		setMomentumAnalysis(new AnalysisMomentum());
-		
-		s = s + getPriceOnlyAnalysis().toString(which) + "\n";
-		s = s + getVolumePriceAnalysis().toString(which) + "\n";
-		s = s + getMomentumAnalysis().toString(which) + "\n";
-		
+		setMomentumAnalysis(new AnalysisMomentum());		
+	}
+	
+	public String getAnalysis(int which) {
+		String s = "";
+		s = s + getPriceAnalysis(which) + "\n";
+		s = s + getVolumeAnalysis(which) + "\n";
+		s = s + getMomentumAnalysis(which) + "\n";
 		return s;
 	}
 	
-	// TODO take only complete candles; ignore opening / close high volume?
-
-//	public boolean isLastCandleNormal() { 
-//		boolean normal=true;
-//		Integer last = getPrices().size()-1;
-//		normal = isCandleNormal(last);
-//		return normal;
-//	}
+	public String getPriceAnalysis(int which) {
+		String s = "";
+		s = s + getPriceOnlyAnalysis().toString(which) + "\n";		
+		return s;
+	}
 	
-//	public boolean isCandleNormal(Integer which) { // VWAP-based
-//		boolean normal = false;
-//		Candle candle = getPrices().get(which);
-//		String s="";
-//		if(candle.isVWAPSpreadAboveNormal() && candle.isSizeAboveNormal()) {
-//			return true;
-//		} 
-//		if(candle.isVWAPSpreadBelowNormal() && candle.isSizeBelowNormal()) {
-//			return true;
-//		} 
-//		if(candle.isVWAPSpreadNormal() && candle.isSizeNormal()) {
-//			return true;
-//		} 
-//		System.out.println("Price-Volume anomaly(candle): VWAP");
-//		return normal;
-//	}
+	public String getVolumeAnalysis(int which) {
+		String s = "";
+		s = s + getVolumePriceAnalysis().toString(which) + "\n";		
+		return s;
+	}
 	
-//	public boolean isLastPriceSpreadNormal() { 
-//		boolean normal=true;
-//		normal = getLastCandle().isPriceSpreadNormal();
-//		return normal;
-//	}
-//	public boolean isLastVWAPSpreadNormal() {
-//		boolean normal=true;
-//		Candle last = getPrices().get(getPrices().size()-1);
-//		normal = getLastCandle().isVWAPSpreadNormal();		
-//		return normal;		
-//	}
-//	public boolean isLastSizeNormal() {
-//		boolean normal=true;
-//		Candle last = getPrices().get(getPrices().size()-1);
-//		normal = getLastCandle().isSizeNormal();				
-//		return normal;
-//	}
-//	private Candle getLastCandle() {
-//		Candle last;
-//		last=getPrices().get(getPrices().size()-1);
-//		return last;
-//	}
-//
-//	public Double getMeanSize() {
-//		Double mean = 0.0;
-//		List<Double> sizes = new ArrayList<Double>();
-//		setSizeStats(new Stats(sizes));
-//		for(Candle candle:getPrices()) {
-//			Integer size = candle.getSizeTotal();	
-//			sizes.add(size*1.0);
-//		}
-//		mean = getSizeStats().getMean();		
-//		return mean;
-//	}
-//	
-//	public Double getMeanPriceSpread() {
-//		Double mean = 0.0;
-//		List<Double> spreads = new ArrayList<Double>();
-//		setPriceSpreadStats(new Stats(spreads));
-//		for(Candle candle:getPrices()) {
-//			Double spread = candle.getPriceSpread();		
-//			spreads.add(spread);
-//		}
-//		mean = getPriceSpreadStats().getMean();
-//		return mean;
-//	}
-//
-//	public Double getMeanVWAPSpread() {
-//		Double mean = 0.0;
-//		List<Double> spreads = new ArrayList<Double>();
-//		setVWAPSpreadStats(new Stats(spreads));
-//		for(Candle candle:getPrices()) {
-//			Double spread = candle.getVWAPSpread();
-//			spreads.add(spread);
-//		}
-//		mean = getVWAPSpreadStats().getMean();
-//		return mean;
-//	}
-//	
-//	public Double getMeanPriceSpreadToSizeRatio() {
-//		Double mean=0.0;
-//		List<Double> ratios = new ArrayList<Double>();
-//		setPriceSpreadToSizeRatioStats(new Stats(ratios));
-//		for(Candle candle:getPrices()) {
-//			Double ratio = candle.getPriceSpreadToSizeRatio();
-//			ratios.add(ratio);
-//		}	
-//		mean = getPriceSpreadToSizeRatioStats().getMean();
-//		return mean;
-//	}
-//
-//	public Double getMeanVWAPSpreadToSizeRatio() {
-//		Double mean=0.0;
-//		List<Double> ratios = new ArrayList<Double>();
-//		setVWAPSpreadToSizeRatioStats(new Stats(ratios));
-//		for(Candle candle:getPrices()) {
-//			Double ratio = candle.getVWAPSpreadToSizeRatio();
-//			ratios.add(ratio);
-//		}	
-//		mean = getVWAPSpreadToSizeRatioStats().getMean();		
-//		return mean;
-//	}
+	public String getMomentumAnalysis(int which) {
+		String s = "";
+		s = s + getMomentumAnalysis().toString(which) + "\n";		
+		return s;
+	}
+	
 	
 	// PRIVATE
 
