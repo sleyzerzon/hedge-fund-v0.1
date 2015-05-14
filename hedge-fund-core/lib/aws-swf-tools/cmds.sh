08c3410ba1da7ac32d9c36dc97c3c7c275877f47
Aldrin Leal <aldrin@leal.eng.br>
2015-05-14 23:19:12 +0000

    Missing files

---
+++ hedge-fund-core/lib/aws-swf-tools/cmds.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -DgroupId=com.amazonaws -DartifactId=aws-java-sdk-flow-build-tools -Dversion=1.9.34 -Dpackaging=pom -Dfile=aws-java-sdk-flow-build-tools.pom
+mvn install:install-file -DgroupId=com.amazonaws -DartifactId=aws-java-sdk-flow-build-tools -Dversion=1.9.34 -Dpackaging=jar -Dfile=aws-java-sdk-flow-build-tools-1.9.34.jar
