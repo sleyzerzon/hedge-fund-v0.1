7db8f2eddff3d842ab7fe997492601ae047e9b29
pablo <pablo.personal@icloud.com>
2015-06-02 10:06:29 -0700

    renamed HistorianRT to InvestorMain; added InvestorMain to CodeDeploy upstart scripts

--- hedge-fund-core/src/main/java/com/onenow/main/InvestorMain.java
+++ hedge-fund-core/src/main/java/com/onenow/main/InvestorMain.java
@@ -1,49 +1,41 @@
 package com.onenow.main;
 
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.onenow.constant.BrokerMode;
-import com.onenow.constant.Topology;
-import com.onenow.constant.InvApproach;
 import com.onenow.data.InitMarket;
 import com.onenow.data.InvestmentList;
-import com.onenow.execution.BrokerActivityImpl;
 import com.onenow.execution.BrokerInteractive;
 import com.onenow.execution.BusWallSt;
-import com.onenow.instrument.InvestmentIndex;
-import com.onenow.instrument.Underlying;
 import com.onenow.portfolio.Portfolio;
-import com.onenow.portfolio.PortfolioFactory;
 import com.onenow.util.FlexibleLogger;
 import com.onenow.util.TimeParser;
 
+/** 
+ * Gather complete accurate historical market data
+ * @param args
+ */
+
 public class InvestorMain {
-		
+
 	private static Portfolio marketPortfolio = new Portfolio();
 
-	public static void main(String[] args) throws ParseException, InterruptedException {
-
+	public static void main(String[] args) {
+		
 		FlexibleLogger.setup();
 
 	    // choose relevant timeframe
 	    String toDashedDate = TimeParser.getDashedDatePlus(TimeParser.getDashedToday(), 1);
 
-	    BrokerInteractive broker = new BrokerInteractive(	BrokerMode.PRIMARY, 
+	    BrokerInteractive broker = new BrokerInteractive(	BrokerMode.REALTIME, 
 	    													marketPortfolio, 
 	    													new BusWallSt()); 
-
+			    
 		InitMarket initMarket = new InitMarket(	marketPortfolio, 
 												InvestmentList.getUnderlying(InvestmentList.someStocks), 
 												InvestmentList.getUnderlying(InvestmentList.someIndices),
 												InvestmentList.getUnderlying(InvestmentList.futures), 
 												InvestmentList.getUnderlying(InvestmentList.options),
     											toDashedDate);						
-
+		// register once: get all real-time quotes
 		broker.getLiveQuotes(); 
-		PortfolioFactory portfolioFactory = new PortfolioFactory(broker, marketPortfolio);
-		portfolioFactory.launch();							
 	}
-	
 }
