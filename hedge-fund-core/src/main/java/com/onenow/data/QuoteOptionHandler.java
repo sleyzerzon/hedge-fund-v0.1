faf10b6f8184be3864040574979fc7f6ef47d041
pablo <pablo.personal@icloud.com>
2015-07-21 22:15:14 -0700

    blend in one time series historic and streaming data (BID + BID_OPTION) for options

--- hedge-fund-core/src/main/java/com/onenow/data/QuoteOptionHandler.java
+++ hedge-fund-core/src/main/java/com/onenow/data/QuoteOptionHandler.java
@@ -44,15 +44,15 @@ public class QuoteOptionHandler extends QuoteSharedHandler implements IOptHandle
 		
 		case ASK_OPTION: 
 			Watchr.log(Level.INFO, ">>>>> Ask Option " + optPrice + " for " + investment.toString());
-			MarketPrice.writePriceStreaming(investment, optPrice, PriceType.ASK_OPTION, InvDataSource.IB);
+			MarketPrice.writePriceStreaming(investment, optPrice, PriceType.ASK, InvDataSource.IB);		// ASK_OPTION
 			break;	
 		case BID_OPTION:
 			Watchr.log(Level.INFO, ">>>>> Bid Option " + optPrice + " for " + investment.toString());
-			MarketPrice.writePriceStreaming(investment, optPrice, PriceType.BID_OPTION, InvDataSource.IB);
+			MarketPrice.writePriceStreaming(investment, optPrice, PriceType.BID, InvDataSource.IB);		// BID_OPTION
 			break;
 		case LAST_OPTION:
 			Watchr.log(Level.INFO, ">>>>> Last Option " + optPrice + " for " + investment.toString());
-			MarketPrice.writePriceStreaming(investment, optPrice, PriceType.TRADED_OPTION, InvDataSource.IB);
+			MarketPrice.writePriceStreaming(investment, optPrice, PriceType.TRADED, InvDataSource.IB);	// TRADED_OPTION
 			break;
 
 		case MODEL_OPTION:
